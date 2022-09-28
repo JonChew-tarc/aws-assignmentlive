8eaf9a369f7b184b2ae9c199e51fd4bfaa4ef7ad
JonChewChunWai <77605824+JonChew-tarc@users.noreply.github.com>
2022-09-28 13:31:03 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -102,7 +102,7 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('AddEmpOutput.html', empName = emp_name)
+    return render_template('AddEmpOutput.html', date = datetime.now(), empName = emp_name)
 
 
 if __name__ == '__main__':
